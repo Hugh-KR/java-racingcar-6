07ba698c265ce84b58b500321270b1c9f993ec7e
Hugh <lee2317884@gmail.com>
2023-11-21 22:17:53 +0900

    feat: 자동차 이름 중복되는 경우 예외 처리

--- src/main/java/racingcar/domain/car/Cars.java
+++ src/main/java/racingcar/domain/car/Cars.java
@@ -2,12 +2,14 @@ package racingcar.domain.car;
 
 import java.util.ArrayList;
 import java.util.List;
+import racingcar.domain.car.validator.CarsValidator;
 
 public class Cars {
 
     private final List<Car> cars = new ArrayList<>();
 
     public Cars(final List<String> carNames) {
+        CarsValidator.validateCarNames(carNames);
         carNames.forEach(carName -> cars.add(new Car(carName)));
     }
 }
