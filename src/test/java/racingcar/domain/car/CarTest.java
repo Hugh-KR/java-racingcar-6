22a7af051c6d3df5c213a07a95bf7201dae86cfc
Hugh <lee2317884@gmail.com>
2023-11-21 15:38:40 +0900

    teat: 자동차 이름 NULL 혹은 공백인 경우

--- src/test/java/racingcar/domain/car/CarTest.java
+++ src/test/java/racingcar/domain/car/CarTest.java
@@ -1,12 +1,12 @@
 package racingcar.domain.car;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import racingcar.constant.exception.CustomIllegalArgumentException;
 import racingcar.constant.exception.car.CarNameExceptionStatus;
 
@@ -14,12 +14,33 @@ class CarTest {
 
     private static final String PROVIDER_PATH = "racingcar.domain.car.provider.CarProvider#";
 
+    private void exceptionTest(final String carName, final String message) {
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(CustomIllegalArgumentException.class)
+                .hasMessageContaining(message);
+    }
+
+    @DisplayName("자동차 이름은 NULL이 될 수 없다")
+    @ParameterizedTest
+    @NullSource
+    void carNameNullExceptionTest(final String carName) {
+        final String message = CarNameExceptionStatus.CAR_NAME_IS_NULL.getMessage();
+        exceptionTest(carName, message);
+    }
+
+    @DisplayName("자동차 이름은 공백이 될 수 없다")
+    @ParameterizedTest
+    @EmptySource
+    void carNameBlankExceptionTest(final String carName) {
+        final String message = CarNameExceptionStatus.CAR_NAME_IS_BLANK.getMessage();
+        exceptionTest(carName, message);
+    }
+
     @DisplayName("자동차 이름은 다섯자를 초과할 수 없다")
     @ParameterizedTest
     @MethodSource(PROVIDER_PATH + "provideForExceededLengthExceptionTest")
     void carNameDuplicatedExceptionTest(final String carName) {
-        assertThatThrownBy(() -> new Car(carName))
-                .isInstanceOf(CustomIllegalArgumentException.class)
-                .hasMessageContaining(CarNameExceptionStatus.CAR_NAME_IS_EXCEEDED_LENGTH.getMessage());
+        final String message = CarNameExceptionStatus.CAR_NAME_IS_EXCEEDED_LENGTH.getMessage();
+        exceptionTest(carName, message);
     }
 }
