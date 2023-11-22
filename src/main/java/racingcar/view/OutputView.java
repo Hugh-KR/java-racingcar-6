5bf55d8a6c0e60d59299131994617ea0f22410a6
Hugh <lee2317884@gmail.com>
2023-11-22 11:21:07 +0900

    refactor: 출력 로직 리팩토링
    * 직관성 향상 목적

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -6,10 +6,11 @@ import racingcar.utils.Delimiter;
 
 public class OutputView {
 
-    private static final String PRINT_STATUS_TITLE = "\n실행 결과";
-    private static final String PRINT_WINNER_MESSAGE = "최종 우승자 : ";
+    private static final String PRINT_STATUS_TITLE = "실행 결과";
+    private static final String PRINT_WINNER_MESSAGE = "최종 우승자";
 
     public void printStatusTitle() {
+        printEmptyLine();
         System.out.println(PRINT_STATUS_TITLE);
     }
 
@@ -17,10 +18,18 @@ public class OutputView {
         carStatus.stream()
                 .map(CarDto::toString)
                 .forEach(System.out::println);
-        System.out.println();
+        printEmptyLine();
     }
 
     public void printWinners(final List<String> winnerNames) {
-        System.out.println(PRINT_WINNER_MESSAGE + Delimiter.joinWithComma(winnerNames));
+        System.out.println(
+                Delimiter.concatWithColon(
+                        PRINT_WINNER_MESSAGE,
+                        Delimiter.joinWithComma(winnerNames))
+        );
+    }
+
+    public void printEmptyLine() {
+        System.out.println();
     }
 }
